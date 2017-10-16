f38a7ba768a51dd7e0196dbe1fb2da143e089339
vavanchik2017 <s3mpau@gmail.com>
2017-10-16 19:09:48 +0300

    Updated

---
+++ btx-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Создаем директорию..."
+sleep 2
+mkdir /usr/local/bin/btcc
+echo "Копируем скрипт..."
+sleep 2
+cp btc-c.py /usr/local/bin/btcc 
+echo "Колдуем..."
+sleep 2
+cp btx /usr/local/bin
+chmod +x btx
+echo "Готово! Введите в терминале "btx" "
+
+
